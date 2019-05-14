1fb7c347f231fb4fd0f59a7d5606195a702bc8d0
johnathantng <johnathan.tng@gmail.com>
2019-05-13 17:30:26 -0700

    need update on uri

--- src/containers/Form/Form.js
+++ src/containers/Form/Form.js
@@ -16,7 +16,7 @@ class Form extends Component {
 	formSubmit = (e) => {
 		e.preventDefault()
 
-		this.state({
+		this.setState({
 			buttonText: '...sending'
 		})
 
@@ -26,7 +26,7 @@ class Form extends Component {
 			message: this.state.message
 		}
 
-		axios.post('https://nodejs-express-ov8drfw1w.now.sh', data)
+		axios.post('http://localhost:4444/api/v1', data)
 		.then( res => {
 			this.setState({ sent: true }, this.resetForm())
 		})
@@ -47,14 +47,14 @@ class Form extends Component {
 	render(){
 	return(
 		<form className="contact-form" onSubmit={ (e) => this.formSubmit(e)}>
-		  <label class="message" htmlFor="message-input">Your Message</label>
-		  <textarea onChange={e => this.setState({ message: e.target.value})} name="message" class="message-input" type="text" placeholder="Please write your message here" value={this.state.message} required/>
+		  <label className="message" htmlFor="message-input">Your Message</label>
+		  <textarea onChange={e => this.setState({ message: e.target.value})} name="message" className="message-input" type="text" placeholder="Please write your message here" value={this.state.message} required/>
 
-		  <label class="message-name" htmlFor="message-name">Your Name</label>
-		  <input onChange={e => this.setState({ name: e.target.value})} name="name" class="message-name" type="text" placeholder="Your Name" value={this.state.name}/>
+		  <label className="message-name" htmlFor="message-name">Your Name</label>
+		  <input onChange={e => this.setState({ name: e.target.value})} name="name" className="message-name" type="text" placeholder="Your Name" value={this.state.name}/>
 
-		  <label class="message-email" htmlFor="message-email">Your Email</label>
-		  <input onChange={(e) => this.setState({ email: e.target.value})} name="email" class="message-email" type="email" placeholder="your@email.com" required value={this.state.email} />
+		  <label className="message-email" htmlFor="message-email">Your Email</label>
+		  <input onChange={(e) => this.setState({ email: e.target.value})} name="email" className="message-email" type="email" placeholder="your@email.com" required value={this.state.email} />
 
 		  <div className="button--container">
 		      <button type="submit" className="button button-primary">{ this.state.buttonText }</button>
