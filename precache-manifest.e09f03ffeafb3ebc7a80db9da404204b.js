e01a84c7ebb93ddf92d881044d1d45e0cb708c18
johnathantng <johnathan.tng@gmail.com>
2019-05-15 16:37:42 -0700

    Updates

---
+++ precache-manifest.e09f03ffeafb3ebc7a80db9da404204b.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c12c46e6b0e8767efa873dcd58764da9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "55d942bd12526676a555",
+    "url": "./static/css/main.3c886df6.chunk.css"
+  },
+  {
+    "revision": "16ab0b249139eda54b47",
+    "url": "./static/js/2.e4ea63af.chunk.js"
+  },
+  {
+    "revision": "55d942bd12526676a555",
+    "url": "./static/js/main.52b14269.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "cd56723af1c7525f5125c5476fc9682b",
+    "url": "./static/media/ceramics.cd56723a.png"
+  },
+  {
+    "revision": "dc89c15b12c36cee2e1f223b64737984",
+    "url": "./static/media/crueltyfree.dc89c15b.png"
+  },
+  {
+    "revision": "9291e821c41e907e0c77b629da2ec55d",
+    "url": "./static/media/decor-black.9291e821.png"
+  },
+  {
+    "revision": "e55cdf872a833ab16635db97c15808cc",
+    "url": "./static/media/decor-blue.e55cdf87.png"
+  },
+  {
+    "revision": "1a5991a96ee1839d1eeec6f60d103cc7",
+    "url": "./static/media/hamburger-bite.1a5991a9.png"
+  },
+  {
+    "revision": "cb3d8ee59be462211b299ad2db9d2cc9",
+    "url": "./static/media/hamburger.cb3d8ee5.png"
+  },
+  {
+    "revision": "7da2813bcba8fabcd60f8a7dc988a835",
+    "url": "./static/media/idrebrand-caption.7da2813b.png"
+  },
+  {
+    "revision": "90ebeb44e2af7475b64d84532e357f49",
+    "url": "./static/media/idrebrand.90ebeb44.png"
+  },
+  {
+    "revision": "b3f61b8bb5b5823e047ffdedd0a1604a",
+    "url": "./static/media/kai-column.b3f61b8b.png"
+  },
+  {
+    "revision": "a716020e4747b2fd9966771db5b6cb14",
+    "url": "./static/media/kaibranding.a716020e.png"
+  },
+  {
+    "revision": "b09cfcd3a934a7b835c56b2ce3d17ac7",
+    "url": "./static/media/kaiselfie.b09cfcd3.png"
+  },
+  {
+    "revision": "d66bf132e4e45172e39c41e75ceba99d",
+    "url": "./static/media/letter.d66bf132.png"
+  },
+  {
+    "revision": "0a2f5b6f999373d6b17ec39338d6a50c",
+    "url": "./static/media/linkedin.0a2f5b6f.png"
+  },
+  {
+    "revision": "5267cae3a102ae5cc8e2e617d7bfe851",
+    "url": "./static/media/phone.5267cae3.png"
+  },
+  {
+    "revision": "4593e6cd445166799575a095f503e49f",
+    "url": "./static/media/pickapath-caption.4593e6cd.png"
+  },
+  {
+    "revision": "e318dd8535e514e7305b62807fee7c74",
+    "url": "./static/media/pickapath.e318dd85.png"
+  },
+  {
+    "revision": "dc6a390907baa55385f63b2737b7fabb",
+    "url": "./static/media/slippers-caption.dc6a3909.png"
+  },
+  {
+    "revision": "7894f464e9630d83893109f85d5b4549",
+    "url": "./static/media/slippers.7894f464.png"
+  },
+  {
+    "revision": "a3f5106d1e49942e1047ef79153072e9",
+    "url": "./static/media/voodoo.a3f5106d.png"
+  },
+  {
+    "revision": "a05df173fced143598f654e00456f64e",
+    "url": "./static/media/yelort.a05df173.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-kai/precache-manifest.c39d02f002318f2eb4149d3e6a98e1fa.js"
+  "./precache-manifest.e09f03ffeafb3ebc7a80db9da404204b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-kai/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
